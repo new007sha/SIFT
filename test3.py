9bbfd531d460c483442bcf98f76a5e0bc5a0f2bf
new007sha <new07sha@gmail.com>
2025-12-11 18:58:40 +0330

    Revert "Delete test3.py"
    This reverts commit 29429a3a5cc10a77ca78de31fc47cbbd758d8515.

---
+++ test3.py
@@ -0,0 +1,14 @@
+import cv2
+cap=cv2.VideoCapture(0)
+sift=cv2.SIFT_create()
+while True :
+    ret,frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    Keypoints,descriptors=sift.detectAndCompute(gray,None)
+    frame=cv2.drawKeypoints(frame,Keypoints,frame,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow("mapping",frame)
+    if cv2.waitKey(1)&0xFF==ord(' '):
+        break
+    # 12345
+cap.release()
+cv2.destroyAllWindows()   
